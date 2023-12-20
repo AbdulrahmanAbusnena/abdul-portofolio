2162c7c34d076532a4fa9b11122ef0461dcb4888
absuleahman_abusnea <user.name>
2023-12-20 19:28:39 +0000

    git commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-
+//import 'package:abdul/main.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:abdul/overview.dart';
+
+// import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final name = 'Abdurahman Abusnena';
+  final name = 'Abdulrahman Abusnena';
   final jobTitle = 'Software Developer';
 
   final intro = '';
@@ -28,51 +30,67 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '$name',
+          name,
           style: GoogleFonts.rubik(fontWeight: FontWeight.bold, fontSize: 25),
         ),
         centerTitle: true,
         leading: CircleAvatar(
-          child: SvgPicture.asset(
+          child: Image.asset(
             'assets/ab.jpg',
           ),
         ),
         actions: [
-          TextButton(onPressed: () {}, child: Text('Contact Me')),
-          TextButton(onPressed: () {}, child: Text('About')),
-        ], 
+          TextButton(
+              onPressed: () {
+                // ignore: avoid_print
+                print('Contact Me is Pressed');
+              },
+              child: const Text('Contact Me')),
+          TextButton(
+              onPressed: () {
+                // ignore: avoid_print
+              },
+              child: const Text('About')),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Blog'),
+          )
+        ],
       ),
-      body: Container(
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/abdul.png',
-                  height: 55,
-                  width: 55,
-                ),
-              ],
-            ),
-            Center(
-              child: Text(
-                '$name  - $jobTitle',
-                style: GoogleFonts.rubik(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            //  crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/abdul.png',
+                // height: 55,
+                // width: 55,
               ),
+            ],
+          ),
+          Text(
+            '$name  - $jobTitle',
+            style: GoogleFonts.rubik(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
             ),
-            const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(''),
-              ],
-            )
-          ],
-        ),
+          ),
+          Divider(),
+          Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(overview,
+                        style: GoogleFonts.rubik(
+                            fontSize: 18, fontWeight: FontWeight.normal)),
+                  ]))
+        ],
       ),
     );
   }
