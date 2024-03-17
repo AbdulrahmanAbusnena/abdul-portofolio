e4ead4c87385ecb3fab21c7ae7f906ada52fd8d6
absuleahman_abusnea <user.name>
2024-03-17 00:52:35 +0000

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:abdul/Pages/homepage.dart';
 import 'package:abdul/themes_colors/theme.dart';
 
 void main() {
-  runApp( MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-   MainApp({super.key}); 
+ const  MainApp({super.key}); 
 
   @override
   Widget build(BuildContext context) {
